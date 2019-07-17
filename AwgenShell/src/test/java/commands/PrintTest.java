27f8f0ed2bd451f3159b038ae7a3f41d47d0b52f
TheDudeFromCI <thedudefromci@gmail.com>
2019-07-17 13:35:59 -0700

    Added tests for print command.
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

---
+++ AwgenShell/src/test/java/commands/PrintTest.java
@@ -0,0 +1,60 @@
+package commands;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.junit.Test;
+import net.whg.awgenshell.CommandSender;
+import net.whg.awgenshell.Permissions;
+import net.whg.awgenshell.ShellEnvironment;
+
+public class PrintTest
+{
+	@Test
+	public void one_word()
+	{
+		CommandSender sender = mock(CommandSender.class);
+		when(sender.getPermissions()).thenReturn(Permissions.ALL);
+
+		ShellEnvironment shell = new ShellEnvironment(sender);
+
+		shell.runCommand("print Hi");
+		verify(sender).println("Hi");
+	}
+
+	@Test
+	public void two_words()
+	{
+		CommandSender sender = mock(CommandSender.class);
+		when(sender.getPermissions()).thenReturn(Permissions.ALL);
+
+		ShellEnvironment shell = new ShellEnvironment(sender);
+
+		shell.runCommand("print Hello world!");
+		verify(sender).println("Hello world!");
+	}
+
+	@Test
+	public void one_word_withSymbol()
+	{
+		CommandSender sender = mock(CommandSender.class);
+		when(sender.getPermissions()).thenReturn(Permissions.ALL);
+
+		ShellEnvironment shell = new ShellEnvironment(sender);
+
+		shell.runCommand("print Sup ?");
+		verify(sender).println("Sup?");
+	}
+
+	@Test
+	public void no_args()
+	{
+		CommandSender sender = mock(CommandSender.class);
+		when(sender.getPermissions()).thenReturn(Permissions.ALL);
+
+		ShellEnvironment shell = new ShellEnvironment(sender);
+
+		shell.runCommand("print");
+		verify(sender).println("");
+	}
+}
